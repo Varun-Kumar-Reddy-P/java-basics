657930c3125946671b304072bb1b3464625025e5
Varun-Kumar-Reddy-P <pvarunkreddy@gmail.com>
2026-01-26 15:43:03 +0530

    added variables and data types example

---
+++ varaibles/DataTypeDemo.java
@@ -0,0 +1,25 @@
+public class DataTypeDemo {
+    public static void main(String[] args) {
+
+        // Demonstrating different data types in Java
+        byte myByte = 127;
+        short myShort = 5000;
+        int myInt = 5;
+        long myLong = 15000000L;
+        float myFloat = 5.99f;
+        double myDouble = 3.14159265359;
+        boolean myBoolean = true;
+        char myChar = 'd';
+        String myString = "Hello";
+
+        System.out.println("Byte value: " + myByte);
+        System.out.println("Short value: " + myShort);
+        System.out.println("Int value: " + myInt);
+        System.out.println("Long value: " + myLong);
+        System.out.println("Float value: " + myFloat);
+        System.out.println("Double value: " + myDouble);
+        System.out.println("Boolean value: " + myBoolean);
+        System.out.println("Char value: " + myChar);
+        System.out.println("String value: " + myString);
+    }
+}
